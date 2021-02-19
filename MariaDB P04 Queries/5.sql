bde816454e1afd5cb7acfd064949ccc13bfe4bc6
Yuki CS <34201397+Yukics@users.noreply.github.com>
2021-02-19 18:03:37 +0100

    Add files via upload

--- MariaDB P04 Queries/5.sql
+++ MariaDB P04 Queries/5.sql
@@ -3,5 +3,5 @@ use P04_queries01;
 #This query is impossible if there is not NULl record on Occupations due to that im just comparing with occu_code
 select concat(e.name, ' ', e.surname) as FullName, o.name from EMPLOYEES as e
 left outer join OCCUPATIONS as o on e.occu_code = o.code
-where (e.occu_code not in ('MAN','EMP') or e.occu_code is null) and e.salary > 2000
+where (o.name not in ('MANAGER','EMPLOYEE') or e.occu_code is null) and e.salary > 2000
 order by o.name, FullName;
