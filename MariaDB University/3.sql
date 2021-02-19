545fe71cd33df4f77750f042956441ec79c2c8bb
Yuki CS <34201397+Yukics@users.noreply.github.com>
2021-02-19 16:02:30 +0100

    Delete MariaDB University directory

--- MariaDB University/3.sql
+++
@@ -1,5 +0,0 @@
-use P04_queries01;
-
-select concat(e.name, ' ', e.surname) as FullName from EMPLOYEES as e
-where occu_code is null and dept_num is null
-order by FullName;
--- MariaDB University/5.sql
+++
@@ -1,6 +0,0 @@
-use P04_queries01;
-
-#This query is impossible if there is not NULl record on Occupations due to that im just comparing with occu_code
-select concat(e.name, ' ', e.surname) as FullName, o.name from EMPLOYEES as e
-left outer join OCCUPATIONS as o on o.code = e.occu_code
-where (e.occu_code not in ('MAN','EMP') or e.occu_code is null) and e.salary > 2000
