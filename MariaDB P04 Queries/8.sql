12692d54540c159069f71930c9153f24f348c179
Yuki CS <34201397+Yukics@users.noreply.github.com>
2021-02-19 18:00:01 +0100

    Add files via upload

--- MariaDB P04 Queries/8.sql
+++ MariaDB P04 Queries/8.sql
@@ -1,7 +1,7 @@
 use P04_queries01;
 
-select D.name, count(dept_num)from EMPLOYEES
-inner join DEPARTMENTS D on EMPLOYEES.dept_num = D.num
+select D.name, count(*) as num_employees from EMPLOYEES E
+right outer join DEPARTMENTS D on E.dept_num = D.num
 group by dept_num
 union
 select dept_num, count(*) from EMPLOYEES
